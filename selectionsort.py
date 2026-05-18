a9c0f668c61137fa4b60a14029ed82977eedb066
sohamw10 <sohamw102005@gmail.com>
2026-05-18 15:03:57 +0530

    Add files via upload

---
+++ selectionsort.py
@@ -0,0 +1,20 @@
+arr=[]
+
+n= int(input("enter the number of element"))
+
+for i in range(n):
+    num = int(input("enter the number of element"))
+    arr.append(num)
+
+
+for i in range(n):
+    min_index =i
+
+for j in range(n+1, i):
+      if arr[j] < arr[ min_index]:
+      min_index =j
+
+  arr[i] , arr[min_index] = arr[min_index] , arr[i]
+
+print("sorted array")
+print(arr)
